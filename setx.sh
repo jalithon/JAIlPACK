9cb3d68ed781457d6dc9acf66b924d2ba04c1c8f
Teamthon <84296580+Teamthon@users.noreply.github.com>
2021-05-18 21:37:05 +0300

    Update setx.sh

--- setx.sh
+++ setx.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "
-          _  __    __  __
-         | |/ /___|  \/  | ___
-         | ' // _ \ |\/| |/ _ \
-         | . \  __/ |  | | (_) |
-         |_|\_\___|_|  |_|\___/
+          Sadthon
 
-        𓍹 Visit @rruuurr for help 𓍻
+        𓍹 Visit @sadthon for help 𓍻
 "
 
 python3 -m userbot
